f6ad9aefc860c333384f9ba4d8f75a4ac68730ec
JiaxinZHANG97 <635622125@qq.com>
2020-11-24 23:46:46 -0500

    Update LNCaP_example.py

--- flimanalyzer/MLfeatureanalyzer/LNCaP_example.py
+++ flimanalyzer/MLfeatureanalyzer/LNCaP_example.py
@@ -11,4 +11,5 @@ t1 = ['FAD a1', 'FAD a2', 'FAD photons', 'FAD t1', 'FAD t2', 'NAD(P)H a1', 'NAD(
 # t2 = ['FAD a1[%]', 'FAD photons', 'FAD t1', 'FAD t2', 'NAD(P)H a1[%]', 'NAD(P)H photons', 'NAD(P)H t1', 'NAD(P)H t2']
 
 lncap = LNCaPSingleCell('LNCaP', 'Master-N_PhotFilter350-090620-new.csv', 'prostate_norm_relu_sae_g1_5feature.pkl', t1)
+lncap.apply_model()
 lncap.create_excel()
