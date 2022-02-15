88a5febf091cd838e1a922ebd32acffdca444f3a
MrMabogo <brianmsbsdcs@gmail.com>
2022-02-15 12:05:20 -0500

    indent fixes

--- flim/gui/datapanel.py
+++ flim/gui/datapanel.py
@@ -197,6 +197,9 @@ class PandasFrame(wx.Frame):
         self.dataview = data
         self.showcolindex = showcolindex
         # get all columns that define categories; these are the columns with view filters
+        print(groups)
+        print(data.select_dtypes(['category']).columns.values)
+        print(data.select_dtypes(['category']).columns.get_level_values(0).values)
         if groups is None:
             groups = list(data.select_dtypes(['category']).columns.values)
             groups = data.select_dtypes(['category']).columns.get_level_values(0).values
@@ -323,17 +326,17 @@ class PandasFrame(wx.Frame):
     
     def apply_filters(self, filtercfg, showdiscarded=False):
         if len (filtercfg) == 0:
-    	    cfg, keys = self.config.get([CONFIG_FILTERS,CONFIG_RANGEFILTERS], returnkeys=True)
-    	    filternames = [f['name'] for f in cfg]
-    	    for fname in filternames:
-    	        if self.droppedrows.get(fname) is not None:
-    	            del self.droppedrows[fname]
+            cfg, keys = self.config.get([CONFIG_FILTERS,CONFIG_RANGEFILTERS], returnkeys=True)
+            filternames = [f['name'] for f in cfg]
+            for fname in filternames:
+                if self.droppedrows.get(fname) is not None:
+                    del self.droppedrows[fname]
         for fname in filtercfg:
             filter = RangeFilter(params=filtercfg[fname])
             if filter.is_selected():
                 self.droppedrows[fname] = filter.get_dropped(self.data)
             elif self.droppedrows.get(fname) is not None:
-            	del self.droppedrows[fname]
+                del self.droppedrows[fname]
         self.modified = True
         self.update_view(showdiscarded=showdiscarded)
             
