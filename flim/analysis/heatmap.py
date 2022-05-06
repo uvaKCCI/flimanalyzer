952e147aa9cdfe0bee5e32fefa6aaf27ea6b5a98
MrMabogo <brianmsbsdcs@gmail.com>
2022-05-06 17:28:27 -0400

    heatmap options

--- flim/analysis/heatmap.py
+++ flim/analysis/heatmap.py
@@ -11,6 +11,35 @@ import matplotlib.pyplot as plt
 from importlib_resources import files
 import flim.resources
 
+class HeatmapConfigDlg(BasicAnalysisConfigDlg):
+    def __init__(self, parent, title, data, selectedgrouping, selectedfeatures, corr_type, numbers):
+        self.corr_type = corr_type
+        self.numbers = numbers
+        BasicAnalysisConfigDlg.__init__(self, parent, title, data, selectedgrouping=selectedgrouping, selectedfeatures=selectedfeatures)
+
+    def get_option_panels(self):
+        corrsizer = wx.BoxSizer(wx.HORIZONTAL)
+        correlation_opts = ['pearson', 'kendall', 'spearman']
+        sel_corr = self.corr_type
+        if sel_corr not in correlation_opts:
+            sel_corr = correlation_opts[0]
+        self.correlation_combobox = wx.ComboBox(self.panel, wx.ID_ANY, style=wx.CB_READONLY, value=sel_corr, choices=correlation_opts)
+        corrsizer.Add(wx.StaticText(self.panel, label="Correlation Method "), 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5)
+        corrsizer.Add(self.correlation_combobox, 0, wx.ALL|wx.EXPAND|wx.ALIGN_CENTER_VERTICAL, 5)
+
+        colorsizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.num_checkbox = wx.CheckBox(self.panel, wx.ID_ANY, "Include Numbers ")
+        self.num_checkbox.SetValue(self.numbers)
+        colorsizer.Add(self.num_checkbox, 0, wx.ALL|wx.EXPAND|wx.ALIGN_CENTER_VERTICAL, 5)
+
+        return [corrsizer, colorsizer]
+
+    def _get_selected(self):
+        params = super()._get_selected()
+        params['corr_type'] = self.correlation_combobox.GetValue()
+        params['numbers'] = self.num_checkbox.GetValue()
+        return params
+
 class Heatmap(AbstractAnalyzer):
 
     def __init__(self, data, **kwargs):
@@ -27,6 +56,14 @@ class Heatmap(AbstractAnalyzer):
         source = files(flim.resources).joinpath('heatmap.png')
         return wx.Bitmap(str(source))        
         
+    def get_default_parameters(self):
+        params = super().get_default_parameters()
+        params.update({
+            'corr_type': 'pearson',
+            'numbers': False
+        })
+        return params
+
     def get_required_categories(self):
         return ['any']
 
@@ -36,7 +73,7 @@ class Heatmap(AbstractAnalyzer):
     def run_configuration_dialog(self, parent, data_choices={}):
         selgrouping = self.params['grouping']
         selfeatures = self.params['features']
-        dlg = BasicAnalysisConfigDlg(parent, f'Configuration: {self.name}', self.data, selectedgrouping=selgrouping, selectedfeatures=selfeatures)
+        dlg = HeatmapConfigDlg(parent, f'Configuration: {self.name}', self.data, selectedgrouping=selgrouping, selectedfeatures=selfeatures, corr_type=self.params["corr_type"], numbers=self.params["numbers"])
         if dlg.ShowModal() == wx.ID_OK:
             results = dlg.get_selected()
             self.params.update(results)
@@ -47,7 +84,7 @@ class Heatmap(AbstractAnalyzer):
     def execute(self):
         data_c = self.data[self.params['features']]
         results = {}
-        corr = data_c.corr()
+        corr = data_c.corr(method=self.params['corr_type'])
         fig, ax = plt.subplots(constrained_layout=True)
         ax = sns.heatmap(
             corr,
@@ -55,7 +92,7 @@ class Heatmap(AbstractAnalyzer):
             vmin=-1, vmax=1, center=0,
             cmap=sns.diverging_palette(20, 220, n=200),
             square=True,
-            annot=True
+            annot=self.params['numbers']
         )
         ax.set_yticklabels(
             ax.get_yticklabels(),
