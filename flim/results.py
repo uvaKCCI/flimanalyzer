2f3947d8b5a9d0de73d0395865079fa5861b7324
MrMabogo <brianmsbsdcs@gmail.com>
2022-10-14 13:43:56 -0400

    account for colon space pattern

--- flim/results.py
+++ flim/results.py
@@ -40,45 +40,36 @@ class LocalResultClear(LocalResult):
                     # if indx was flattened in analyzer.summarize_data, multiindex col values were joined with '\n'--> revert here
                     v.columns = [c.replace("\n", " ") for c in v.columns.values]
                     dirpath = repr(os.path.join(cleardir, f"{k}.txt"))
-                    start = re.match("^\W{0,2}\w", dirpath).end() - 1
-                    end = re.search("\w\W{0,2}$", dirpath).start() + 1
                     drive = dirpath[1:3]
-                    v.to_csv(
-                        drive
-                        + dirpath[3:-1]
+                    savepath = (
+                        dirpath[3:-1]
                         .replace("%", "pcent")
-                        .replace(":", "_")
-                        .replace("\r", "")
-                        .replace("\n", " "),
-                        index=False,
-                        sep="\t",
-                    )
-                elif isinstance(v, matplotlib.figure.Figure):
-                    # v.tight_layout()
-                    dirpath = repr(os.path.join(cleardir, f"{k}.png"))
-                    start = re.match("^\W{0,2}\w", dirpath).end() - 1
-                    end = re.search("\w\W{0,2}$", dirpath).start() + 1
-                    drive = dirpath[1:3]
-                    v.savefig(
-                        drive
-                        + dirpath[3:-1]
-                        .replace("%", "pcent")
-                        .replace(":", "_")
                         .replace("\r", "")
                         .replace("\n", " ")
                     )
+                    savepath = re.sub(":\s?", "_", savepath)
+                    v.to_csv(drive + savepath)
+                elif isinstance(v, matplotlib.figure.Figure):
+                    # v.tight_layout()
+                    dirpath = repr(os.path.join(cleardir, f"{k}.png"))
+                    drive = dirpath[1:3]
+                    savepath = (
+                        dirpath[3:-1]
+                        .replace("%", "pcent")
+                        .replace("\r", "")
+                        .replace("\n", " ")
+                    )
+                    savepath = re.sub(":\s?", "_", savepath)
+                    v.savefig(drive + savepath)
                 else:
                     dirpath = repr(os.path.join(cleardir, f"{k}"))
-                    start = re.match("^\W{1,2}\w", dirpath).end() - 1
-                    end = re.search("\w\W{1,2}$", dirpath).start() + 1
                     drive = dirpath[1:3]
-                    dump(
-                        v,
-                        drive
-                        + dirpath[3:-1]
+                    savepath = (
+                        dirpath[3:-1]
                         .replace("%", "pcent")
-                        .replace(":", "_")
                         .replace("\r", "")
-                        .replace("\n", " "),
+                        .replace("\n", " ")
                     )
+                    savepath = re.sub(":\s?", "_", savepath)
+                    dump(v, drive + savepath)
         return new
