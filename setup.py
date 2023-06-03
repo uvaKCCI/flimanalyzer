5761b75ae5d14c131b4aee3a27908bd48e1b4ae7
ksiller <ksiller@users.noreply.github.com>
2023-06-03 08:51:01 -0400

    updated version tracking

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 import setuptools
-import flim
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = flim.__version__
+#version = get_version(root='.', relative_to=__file__)
 
 setuptools.setup(
-    name="flimanalyzer", # Replace with your own username
-    version=version,
+    name="flimanalyzer",  # Replace with your own username
+    #version=version,
     author="Karsten Siller",
     author_email="khsiller@gmail.com",
     description="FLIM Analyzer",
@@ -16,15 +15,15 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/uvaKCCI/flimanalyzer",
     packages=setuptools.find_packages(),
-    package_data = {'':['resources']},
-    include_package_data = True,
-    entry_points = {
-        "console_scripts": ['flimanalyzer = flim.analyzerapp:main']
-        },    
+    package_data={"": ["resources"]},
+    include_package_data=True,
+    entry_points={"console_scripts": ["flimanalyzer = flim.analyzerapp:main"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
 )
